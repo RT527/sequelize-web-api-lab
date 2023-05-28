ee1b81bf77fd47cab613981286f6a894962b3d2a
RT527 <rafi.talukder7@gmail.com>
2023-05-28 13:49:03 -0400

    update and index

--- controllers/inventors.js
+++ controllers/inventors.js
@@ -9,6 +9,29 @@ const create = async (req, res) => {
   }
 }
 
+const index = async (req, res) => {
+  try {
+    const inventors = await Inventor.findAll()
+    res.status(200).json(inventors)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const inventor = await Inventor.update(
+      req.body,
+      { where: { id: req.params.inventorId }, returning: true }
+    )
+    res.status(200).json(inventor)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
   create,
+  index,
+  update,
 }
--- routes/inventors.js
+++ routes/inventors.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const inventorsCtrl = require('../controllers/inventors.js')
 
 router.post('/', inventorsCtrl.create)
+router.get('/', inventorsCtrl.index)
+router.put('/:inventorId', inventorsCtrl.update)
 
 module.exports = router
